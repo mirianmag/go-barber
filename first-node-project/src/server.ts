a2049adbbadc4298a660bfe129c1de0be2a4fb9c
Mirian Magri <magri.mirian@gmail.com>
2020-07-07 18:00:20 -0400

    config:configuring debbug

--- first-node-project/src/server.ts
+++ first-node-project/src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (resquest, response) => {
-  return response.json({ message: 'Hello Mary' });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🌼 Server started on port 3333');

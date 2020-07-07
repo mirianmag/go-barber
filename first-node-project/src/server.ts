da66a0f2efbf7fc7204ebc101d28d5cbfbc6eb20
Mirian Magri <magri.mirian@gmail.com>
2020-07-07 17:13:13 -0400

    plugins: configuring editors

--- first-node-project/src/server.ts
+++ first-node-project/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
-const app= express();
+const app = express();
 
-app.get('/', (request, response) =>{
-  return response.json({ message: 'Hello Mary'});
-})
+app.get('/', (resquest, response) => {
+  return response.json({ message: 'Hello Mary' });
+});
 
 app.listen(3333, () => {
-  console.log('🌼Server started on port 3333');
+  console.log('🌼 Server started on port 3333');
 });
